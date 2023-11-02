c6226917607498523ceeb162e967e7f8e705102b
Jack-Eddy <144264179+Jack-Eddy@users.noreply.github.com>
2023-11-01 20:08:32 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -7,8 +7,8 @@
 
 // varriables
 let neopixelStrip: neopixel.Strip = null
-let countdown = 4
-let neopixelAmount = countdown
+let countdown: number = 4
+let neopixelAmount: number = 0
 
 // cleanup
 basic.clearScreen()
@@ -22,17 +22,18 @@ neopixelStrip.show()
 input.onButtonPressed(Button.A, function () {
   // setup
   basic.clearScreen()
-  countdown = 4
-  neopixelStrip = neopixel.create(DigitalPin.P0, 4, NeoPixelMode.RGB)
   neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
   neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
   neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
   neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+  countdown = 4
   while (countdown >= 0) {
     basic.pause(500)
     basic.showNumber(countdown)
     neopixelStrip.show()
     countdown = countdown - 1
+    neopixelStrip.setPixelColor(neopixelAmount, neopixel.colors(NeoPixelColors.Black))
+    neopixelAmount = neopixelAmount + 1
   }
   basic.showIcon(IconNames.Happy)
 })
