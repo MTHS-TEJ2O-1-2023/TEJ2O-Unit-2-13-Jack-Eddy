71243892d609ce1d9dd896210a0ee58624351e3f
Jack-Eddy <144264179+Jack-Eddy@users.noreply.github.com>
2023-11-02 02:07:46 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -5,6 +5,33 @@ This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+import neopixel
+
+# variables
+countdown = 4
+neopixel_number = 0
+
+# cleanup
+np = neopixel.NeoPixel(pin16, 4)
+np[0] = (0, 0, 0)
+np[1] = (0, 0, 0)
+np[2] = (0, 0, 0)
+np[3] = (0, 0, 0)
+np.show()
 
 
-display.scroll("Hello, World!")
+if button_a.is_pressed():
+    # setup
+    countdown = 4
+    neopixel_number = 0
+    np[0] = (0, 1, 0)
+    np[1] = (0, 1, 0)
+    np[2] = (0, 1, 0)
+    np[3] = (0, 1, 0)
+    # true loop
+    while (countdown >= 0):
+        sleep(500)
+        display.show(countdown)
+        np.show()
+        countdown = countdown - 1
+        np[ne] = (0, 0, 0)
